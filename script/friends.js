aaa828b507f02a6c54946a9f240eadf3d7643459
Déborah <deborah.bude@gmail.com>
2024-11-14 15:08:23 +0100

    Drag and drop function

--- script/friends.js
+++ script/friends.js
@@ -1,10 +1,50 @@
 import { getUsersData } from "./dataLoader.js";
 import { formatDate } from "./utils.js";
-const friendCard = document.querySelector(".friends__list");
+const friendList = document.querySelector(".friends__list");
+const sortFilterOption = document.getElementById('sort-options')
 
 const allFriends = await getUsersData();
-let listFriends = [];
 
+/* Drag and Drop */
+function initDrapAndDrop() {
+    const friendCard = document.querySelectorAll(".friend__card");
+    let draggedItem;
+    let dragStartClientY;
+
+
+    /** @type {HTMLAnchorElement[]} */
+    friendCard.forEach(card => {
+        card.addEventListener("dragstart", (event)=> {
+            draggedItem = card;
+            event.dataTransfer.effectAllowed = "move"
+            event.target.classList.add("dragging");
+            dragStartClientY = event.clientY;
+        })
+
+        card.addEventListener("dragend", ()=> {
+            draggedItem = null;
+            event.target.classList.remove("dragging");
+        })
+
+        card.addEventListener("dragover", (event) => {
+            event.preventDefault();
+            const box = card.getBoundingClientRect();
+            const offsetY = event.clientY - box.top - box.height / 2;
+            
+            if (card !== draggedItem) {
+                if (offsetY < 0) {
+                    card.parentNode.insertBefore(draggedItem, card);
+                } else {
+                    card.parentNode.insertBefore(draggedItem, card.nextSibling);
+                }
+            }
+        })
+    });
+}
+
+initDrapAndDrop(); 
+
+/* Filter friend by firstName or lastName */
 function sortFriendsBy(property) {
     if(property === "none") {
         return [...allFriends];
@@ -26,28 +66,23 @@ function sortFriendsBy(property) {
 };
 
 function friendListTemplate(sortedFriends) {
-    const newFriend = sortedFriends.map((friend) => {
-        return(
-            `<li class="friend__card">
-                <img class="friend__move-icon" src="./assets/icons/drag-drop.svg" alt="Déplacer">
-                <div class="friend__info">
-                    <div class="friend__profile">
-                        <img src="assets/images/profiles/${friend.profilePicture}" alt="Profil de Alice" class="friend__profile-pic">
-                        <p class="friend__profile-name">${friend.firstName} ${friend.lastName}</p>
-                    </div>
-                    <span class="friends__last-seen">Dernière connexion : il y a ${formatDate(friend.lastConnexion)}</span>
+    return sortedFriends.map((friend) => 
+        `<li class="friend__card" draggable="true">
+            <img class="friend__move-icon" src="./assets/icons/drag-drop.svg" alt="Déplacer">
+            <div class="friend__info">
+                <div class="friend__profile">
+                    <img src="assets/images/profiles/${friend.profilePicture}" alt="Profil de Alice" class="friend__profile-pic">
+                    <p class="friend__profile-name">${friend.firstName} ${friend.lastName}</p>
                 </div>
-                <a href="./messaging.html?id=${friend.conversationId}" class="friend__message"><img src="assets/icons/send.svg" alt="Envoyer un message"></a>
-            </li>`
-        )
-    })
+                <span class="friends__last-seen">Dernière connexion : il y a ${formatDate(friend.lastConnexion)}</span>
+            </div>
+            <a href="./messaging.html?id=${friend.conversationId}" class="friend__message"><img src="assets/icons/send.svg" alt="Envoyer un message"></a>
+        </li>`
+    ).join(''); // Joindre les éléments sans espace entre eux
+}
 
-    listFriends.push(newFriend);
-};
-
-document.getElementById('sort-options').addEventListener('change', (event) => {
-    listFriends = [];
-    const sortedFriends = sortFriendsBy(event.target.value)
-    friendListTemplate(sortedFriends)
-    friendCard.innerHTML = listFriends.join("")
+sortFilterOption.addEventListener('change', (event) => {
+    const sortedFriends = sortFriendsBy(event.target.value);
+    friendList.innerHTML = friendListTemplate(sortedFriends);
+    initDrapAndDrop(); 
 });
