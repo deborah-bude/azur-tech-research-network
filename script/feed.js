1a05a7dfb2f32828a9917c5634abdfadf659e864
Déborah <deborah.bude@gmail.com>
2024-11-14 15:09:50 +0100

    Manage post with class, comment and add JS Doc

--- script/feed.js
+++ script/feed.js
@@ -5,123 +5,177 @@ const feedSection = document.getElementById("feed");
 const popupSection = document.getElementById("popup");
 const popupImage = document.querySelector('.popup__image');
 const popupClose = document.querySelector('.popup__close');
-const postsData = await getPostsData();
-const usersData = await getUsersData();
-let allPosts = [];
 
-//Post generate
-function postTemplate(post) {
-    const {authorId, content, image, reactions, comments, timestamp} = post;
-    const author = usersData.find((user) => user.id === authorId);
-    const datePost = formatDate(timestamp);
+/**
+ * Manage Posts Feed
+ */
+class FeedManager {
+    constructor() {
+        this.postsData = [];
+        this.usersData = [];
+        this.allPosts = [];
+    }
 
-    const allComments = comments.map((comment) => {
-        const commentDate = formatDate(comment.timestamp);
-        const commentAuthor = usersData.find((user) => user.id === comment.authorId);
+    /**
+     * Charge les données des posts et des utilisateurs, puis génère le fil d'actualités.
+     */
+    async loadData() {
+        this.postsData = await getPostsData();
+        this.usersData = await getUsersData();
 
-        return(
-        `<div class="post__comment">
-            <a class="post__comment-user-info" href="#">
-                <img src="assets/images/profiles/${commentAuthor.profilePicture}" alt="Profil de ${commentAuthor.firstName} ${commentAuthor.lastName}" class="post__comment-pic">
-                <p class="post__comment-username">${commentAuthor.firstName} ${commentAuthor.lastName}</p>
-            </a>
-            <div class="post__comment-content">
-                <p class="post__comment-text">${comment.content}</p>
-                <p class="post__comment-date">Publié le ${commentDate}</p>
-                <button class="button">Répondre</button>
+        this.postsData.map((post) => this.postTemplate(post));
+        feedSection.innerHTML = this.allPosts.join("");
+        this.initParticleAnimations();
+        this.initPopupImageView();
+    }
+
+    /**
+     * Create HTML post with template
+     * @param {Object} post - Post Data
+     * @param {number} post.id - ID of the post
+     * @param {number} post.authorId - ID of the author
+     * @param {string} [post.content] - Content of the post (optional)
+     * @param {string} [post.image] - Image URL of the post (optional)
+     * @param {Object} post.reactions - Reactions data of the post
+     * @param {number} post.reactions.like - Number of likes for the post
+     * @param {number} post.reactions.dislike - Number of dislikes for the post
+     * @param {Array.<Object>} post.comments - Comments on the post
+     * @param {number} post.comments[].id - ID of the comment
+     * @param {number} post.comments[].authorId - ID of the comment's author
+     * @param {string} post.comments[].content - Content of the comment
+     * @param {string} post.comments[].timestamp - Timestamp of the comment
+     * @param {string} post.timestamp - Timestamp of the post
+     */
+    postTemplate(post) {
+        const {id, authorId, content, image, reactions, comments, timestamp} = post;
+        const author = this.usersData.find((user) => user.id === authorId);
+        const datePost = formatDate(timestamp);
+    
+        const allComments = comments.map((comment) => {
+            const commentDate = formatDate(comment.timestamp);
+            const commentAuthor = this.usersData.find((user) => user.id === comment.authorId);
+    
+            return(
+            `<div class="post__comment">
+                <a class="post__comment-user-info" href="#">
+                    <img src="assets/images/profiles/${commentAuthor.profilePicture}" alt="Profil de ${commentAuthor.firstName} ${commentAuthor.lastName}" class="post__comment-pic">
+                    <p class="post__comment-username">${commentAuthor.firstName} ${commentAuthor.lastName}</p>
+                </a>
+                <div class="post__comment-content">
+                    <p class="post__comment-text">${comment.content}</p>
+                    <p class="post__comment-date">Publié le ${commentDate}</p>
+                    <button class="button">Répondre</button>
+                </div>
+            </div>`)
+        });
+    
+        const newPost = 
+        `<article class="post">
+            <div class="post__header">
+                <a class="post__user-info" href="#">
+                    <img src="assets/images/profiles/${author.profilePicture}" alt="Profil de ${author.firstname} ${author.lastname}" class="post__profile-pic">
+                    <p class="post__username">${author.firstName} ${author.lastName}</p>
+                </a>
+                <p class="post__date">Publié le ${datePost}</p>
             </div>
-        </div>`)
-    });
-
-    const newPost = 
-    `<article class="post">
-        <div class="post__header">
-            <a class="post__user-info" href="#">
-                <img src="assets/images/profiles/${author.profilePicture}" alt="Profil de ${author.firstname} ${author.lastname}" class="post__profile-pic">
-                <p class="post__username">${author.firstName} ${author.lastName}</p>
-            </a>
-            <p class="post__date">Publié le ${datePost}</p>
-        </div>
-        ${content ? `<p class="post__content">${content}</p>` : ""}
-        ${image ? `<img src="assets/images/posts/${image}" alt="Image de ${image} posté par ${author.firstName} ${author.lastName}" class="post__image">` : ""}
-        <section class="post__footer">
-            <div class="post__reactions">
-                <button class="post__reaction">
-                    <img class="post__icon post__icon--share" src="./assets/icons/share.svg" alt="Partager">
-                    <div class="particles"></div>
-                </button>
-                <div>
+            ${content ? `<p class="post__content">${content}</p>` : ""}
+            ${image ? `<img src="assets/images/posts/${image}" alt="Image de ${image} posté par ${author.firstName} ${author.lastName}" class="post__image">` : ""}
+            <section class="post__footer">
+                <div class="post__reactions">
                     <button class="post__reaction">
-                        <img class="post__icon post__icon--like" src="./assets/icons/like.svg" alt="Like">
-                        <div class="particles"></div>
+                        <img class="post__icon post__icon--share" src="./assets/icons/share.svg" alt="Partager">
+                        <span class="particles"></span>
                     </button>
-                    <p>${reactions.like}</p>
+                    <div>
+                        <button class="post__reaction">
+                            <img class="post__icon post__icon--like" src="./assets/icons/like.svg" alt="Like">
+                            <span class="particles"></span>
+                        </button>
+                        <p>${reactions.like}</p>
+                    </div>
+                    <div>
+                        <button class="post__reaction">
+                            <img class="post__icon post__icon--dislike" src="./assets/icons/dislike.svg" alt="Dislike">
+                            <span class="particles"></span>
+                        </button>
+                        <p>${reactions.dislike}</p>
+                    </div>
                 </div>
-                <div>
-                    <button class="post__reaction">
-                        <img class="post__icon post__icon--dislike" src="./assets/icons/dislike.svg" alt="Dislike">
-                        <div class="particles"></div>
-                    </button>
-                    <p>${reactions.dislike}</p>
+                <div class="post__comments-section">
+                    <h3 class="post__comments-title">Commentaires</h3>
+                    <div class="comment-respond">
+                        <label for="comment-postId-${id}">Postez votre commentaire</label>
+                        <input id="comment-postId-${id}" type="text" placeholder="Ecrivez votre commentaire" class="comment-respond__input">
+                        <button type="submit" class="button">Répondre</button>
+                    </div>
+                    ${allComments.join("")}
                 </div>
-            </div>
-            <div class="post__comments-section">
-                <h3 class="post__comments-title">Commentaires</h3>
-                <div class="comment-respond">
-                    <label for="comment">Postez votre commentaire</label>
-                    <input id="comment" type="text" placeholder="Ecrivez votre commentaire" class="comment-respond__input">
-                    <button type="submit" class="button">Répondre</button>
-                </div>
-                ${allComments.join("")}
-            </div>
-        </section>
-    </article>`;
+            </section>
+        </article>`;
+    
+        this.allPosts.push(newPost);
+    }
 
-    allPosts.push(newPost);
-}
-
-postsData.map((post) => postTemplate(post))
-feedSection.innerHTML = allPosts.join("");
-
-//Particule animations
-function particuleAnimation(event) {
-    const btn = event.currentTarget;
-    const particleContainer = btn.querySelector(".particles");
-
-    // // Création particules
-    for (let i = 0; i < 10; i++) {
-        const particle = document.createElement("div");
-        particle.classList.add("particle");
-        
-        particle.style.setProperty('--offset-x', `${Math.random() * 100 - 50}px`);
-        particle.style.setProperty('--offset-y', `${Math.random() * 100 - 50}px`);  
-
-        particleContainer.appendChild(particle);
-
-        // Delete particule at the end of animation
-        particle.addEventListener("animationend", () => {
-            particle.remove();
+    /**
+     * Initialize particles animation.
+     */
+    initParticleAnimations() {
+        document.querySelectorAll(".post__reaction").forEach((reactionButton) => {
+            reactionButton.addEventListener("click", this.particleAnimation.bind(this));
         });
     }
-}
 
-document.querySelectorAll(".post__reaction").forEach((reactionButton) => {
-    reactionButton.addEventListener("click", particuleAnimation);
-});
-
-function popupImgOpen(event) {
-    popup.classList.add('active');
-    popupImage.src = event.currentTarget.src;
-}
-
-//Show image in full screen
-document.querySelectorAll(".post__image").forEach((image) => {
-    image.addEventListener("click", popupImgOpen);
-});
-
-popup.addEventListener('click', (event) => {
-    if (event.target === popupSection || event.target === popupClose) {
-      popup.classList.remove('active');
-      popupImage.src = "";
+    /**
+     * Initialize popup image
+     */
+    initPopupImageView() {
+        document.querySelectorAll(".post__image").forEach((image) => {
+          image.addEventListener("click", this.popupImgOpen.bind(this));
+        });
+    
+        popupSection.addEventListener('click', (event) => {
+          if (event.target === popupSection || event.target === popupClose) {
+            popupSection.classList.remove('active');
+            popupImage.src = "";
+          }
+        });
     }
-});
+
+    /**
+     * Created particles animation with click event on the buttons
+     * @param {Event} event - Click event on reactions buttons
+     */
+    particleAnimation(event) {
+        const btn = event.currentTarget;
+        const particleContainer = btn.querySelector(".particles");
+
+        // Particles creations
+        for (let i = 0; i < 10; i++) {
+            const particle = document.createElement("div");
+            particle.classList.add("particle");
+            
+            particle.style.setProperty('--offset-x', `${Math.random() * 100 - 50}px`);
+            particle.style.setProperty('--offset-y', `${Math.random() * 100 - 50}px`);  
+
+            particleContainer.appendChild(particle);
+
+            // Delete particle at the end of animation
+            particle.addEventListener("animationend", () => {
+                particle.remove();
+            });
+        }
+    }
+    
+    /**
+     * Open popup image with click event
+     * @param {Event} event - Click event on image
+     */
+    popupImgOpen(event) {
+        popup.classList.add('active');
+        popupImage.src = event.currentTarget.src;
+    }
+}
+
+// Utilisation
+const feedManager = new FeedManager();
+await feedManager.loadData();
